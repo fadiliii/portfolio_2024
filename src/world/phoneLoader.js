66ee70743f6d2b7aef26e0dd1f19a56e9b90f472
Nassim <nassimfadili@outlook.fr>
2024-10-26 14:51:24 +0200

    sizing and rotation

--- src/world/phoneLoader.js
+++ src/world/phoneLoader.js
@@ -4,6 +4,11 @@ export default function PhoneLoader(scene, renderer, camera) {
   const gltfloader = new GLTFLoader();
   gltfloader.load("/static/models/phone/scene.gltf", (gltf) => {
     scene.add(gltf.scene);
+    const phoneModel = gltf.scene;
+
+    phoneModel.scale.set(20, 20, 20);
+    phoneModel.rotation.y = Math.PI / 2;
+    phoneModel.position.y = 1.1;
     renderer.render(scene, camera);
   });
 }
