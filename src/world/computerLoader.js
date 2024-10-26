1ddf47af9c19fca198c994b92ad7f14cfe4b003f
Nassim <nassimfadili@outlook.fr>
2024-10-26 14:51:55 +0200

    async function for lil gui add on model

--- src/world/computerLoader.js
+++ src/world/computerLoader.js
@@ -1,9 +1,32 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-export default function ComputerLoader(scene, renderer, camera) {
-  const gltfloader = new GLTFLoader();
-  gltfloader.load("/static/models/pc/scene.gltf", (gltf) => {
-    scene.add(gltf.scene);
-    renderer.render(scene, camera);
+export default async function ComputerLoader(scene, renderer, camera) {
+  return new Promise((resolve, reject) => {
+    const gltfloader = new GLTFLoader();
+
+    gltfloader.load(
+      "/static/models/pc/scene.gltf",
+      (gltf) => {
+        scene.add(gltf.scene);
+        const computerModel = gltf.scene;
+
+        computerModel.scale.set(0.03, 0.03, 0.03);
+        computerModel.rotation.y = Math.PI / 2;
+        computerModel.position.y = -1.56;
+        computerModel.position.x = 1.14;
+
+        resolve(computerModel);
+
+        renderer.render(scene, camera);
+      },
+      undefined,
+      (error) => {
+        console.error(
+          "Une erreur s'est produite lors du chargement du modèle:",
+          error
+        );
+        reject(error);
+      }
+    );
   });
 }
