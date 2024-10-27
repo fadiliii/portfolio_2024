7d0cce148fd0dfabb1d77d2f0989856629b74774
Nassim <nassimfadili@outlook.fr>
2024-10-27 13:21:00 +0100

    EDIT: model tv to sperare meshes in blender

--- src/world/televisionLoader.js
+++ src/world/televisionLoader.js
@@ -2,7 +2,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 export default function TelevisionLoader(scene, renderer, camera) {
   const gltfloader = new GLTFLoader();
-  gltfloader.load("/static/models/tv/scene.gltf", (gltf) => {
+  gltfloader.load("/static/models/tv/scene2.gltf", (gltf) => {
+    console.log(gltf);
     scene.add(gltf.scene);
     gltf.scene.rotation.y = Math.PI / 2;
     renderer.render(scene, camera);
