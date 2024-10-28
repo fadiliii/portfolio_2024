f108078ffee4c90caa092f9d0c20ad651c1b3b75
Nassim <nassimfadili@outlook.fr>
2024-10-28 16:19:01 +0100

    ADD: video texture on television mesh

--- src/world/televisionLoader.js
+++ src/world/televisionLoader.js
@@ -1,11 +1,63 @@
+import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-export default function TelevisionLoader(scene, renderer, camera) {
-  const gltfloader = new GLTFLoader();
-  gltfloader.load("/static/models/tv/scene2.gltf", (gltf) => {
-    console.log(gltf);
-    scene.add(gltf.scene);
-    gltf.scene.rotation.y = Math.PI / 2;
-    renderer.render(scene, camera);
+export function TelevisionLoader(scene, renderer, camera) {
+  const gltfLoader = new GLTFLoader();
+  return new Promise((resolve, reject) => {
+    gltfLoader.load(
+      "/static/models/tv/scene2.gltf",
+      (gltf) => {
+        const TVModel = gltf.scene;
+        TVModel.rotation.y = Math.PI / 2;
+
+        // Trouver le maillage de l'écran
+        const screenMesh = TVModel.getObjectByName("tv_screen");
+
+        // Créer l'élément vidéo
+        const video = document.createElement("video");
+        video.src = "/static/videos/Me at the zoo.mp4";
+        video.muted = true;
+        video.loop = true;
+        video.play();
+
+        // Créer la texture vidéo
+        const videoTexture = new THREE.VideoTexture(video);
+        videoTexture.minFilter = THREE.LinearFilter;
+        videoTexture.magFilter = THREE.LinearFilter;
+        videoTexture.format = THREE.RGBAFormat;
+        videoTexture.center.set(0.5, 0.5);
+
+        if (screenMesh) {
+          // Récupérer les dimensions de l'écran
+          const box = new THREE.Box3().setFromObject(screenMesh);
+          const screenWidth = box.max.x - box.min.x;
+          const screenHeight = box.max.y - box.min.y;
+
+          // Calculer le rapport d'aspect de l'écran
+          const screenAspect = screenWidth / screenHeight;
+
+          // Créer un nouveau matériau pour l'écran
+          const screenMaterial = new THREE.MeshBasicMaterial({
+            map: videoTexture,
+          });
+
+          // Appliquer le matériau à l'écran
+          screenMesh.material = screenMaterial;
+
+          // Ajuster la répétition de la texture vidéo
+          videoTexture.repeat.set(2, 2);
+          videoTexture.offset.set(2, 2);
+
+          // Ajuste les dimensions de la texture vidéo pour éviter le zoom
+          videoTexture.needsUpdate = true;
+        }
+
+        scene.add(TVModel);
+        renderer.render(scene, camera);
+        resolve({ TVModel, videoTexture });
+      },
+      undefined,
+      reject
+    );
   });
 }
