6e53ca7fe67ac11cff601b12113c500a1c58c2e6
Nassim <nassimfadili@outlook.fr>
2024-10-28 16:18:37 +0100

    ADD: phone ajustment

--- src/world/phoneLoader.js
+++ src/world/phoneLoader.js
@@ -1,14 +1,22 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-export default function PhoneLoader(scene, renderer, camera) {
+export function PhoneLoader(scene, renderer, camera) {
   const gltfloader = new GLTFLoader();
-  gltfloader.load("/static/models/phone/scene.gltf", (gltf) => {
-    scene.add(gltf.scene);
-    const phoneModel = gltf.scene;
-
-    phoneModel.scale.set(20, 20, 20);
-    phoneModel.rotation.y = Math.PI / 2;
-    phoneModel.position.y = 1.1;
-    renderer.render(scene, camera);
+  return new Promise((resolve, reject) => {
+    gltfloader.load(
+      "/static/models/phone/scene.gltf",
+      (gltf) => {
+        scene.add(gltf.scene);
+        const phoneModel = gltf.scene;
+        phoneModel.scale.set(20, 20, 20);
+        phoneModel.rotation.y = -Math.PI / 2;
+        phoneModel.position.y = 1.1;
+        phoneModel.visible = false;
+        renderer.render(scene, camera);
+        resolve(phoneModel);
+      },
+      undefined,
+      reject
+    );
   });
 }
